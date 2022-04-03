27e07a065acce2b6f88f50b094d023f8cf9121c7
u16rogue <u16rogue@waifu.club>
2022-04-03 23:13:11 +0800

    added string_literal::match and operator== for string matching

--- include/khuneo/string_literals.hpp
+++ include/khuneo/string_literals.hpp
@@ -5,6 +5,8 @@ namespace khuneo
 	template <typename T, int sz>
 	struct string_literal
 	{
+		using type = T;
+
 		consteval string_literal(const T (&_str)[sz])
 		{
 			for (int i = 0; i < sz; ++i)
@@ -12,6 +14,50 @@ namespace khuneo
 		}
 
 		T str[sz] { 0 };
-		const int length  { sz };
+		const int length  { sz - 1 };
+
+		auto match(const T * other) -> bool
+		{
+			for (int i = 0; i < length; ++i)
+			{
+				if (other[i] != str[i])
+					return false;
+
+				if (i == length - 1)
+					return true;
+			}
+
+			return false;
+		}
+
+		auto operator==(const T * rhs) -> bool
+		{
+			return match(rhs);
+		}
+	};
+
+	// For single characters
+	template <typename T>
+	struct string_literal
+	{
+		using type = T;
+
+		consteval string_literal(const T &_str)
+			: str(_str)
+		{
+		}
+
+		T str { 0 };
+		const int length { 1 };
+
+		auto match(const T * other) -> bool
+		{
+			return str == *other;
+		}
+
+		auto operator==(const T * rhs) -> bool
+		{
+			return match(rhs);
+		}
 	};
 }
