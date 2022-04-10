8338cb29367e4f477653532c330f7d95a2c607bb
u16rogue <u16rogue@waifu.club>
2022-04-10 22:57:06 +0800

    cleaned up for rewrite

--- kh_compiler/include/khuneo/compiler/ast.hpp
+++ kh_compiler/include/khuneo/compiler/ast.hpp
@@ -2,22 +2,4 @@
 
 namespace khuneo::ast
 {
-	struct ast_node
-	{
-		ast_node * node_parent { nullptr };
-		ast_node * node_child  { nullptr };
-
-		ast_node * node_prev   { nullptr };
-		ast_node * node_next   { nullptr };
-
-		int line { -1 };
-		int col  { -1 };
-
-		int token_id { -1 };
-		const char * token_name;
-
-		int    char_sz { -1 }; // very unecessary, we can just template this instead
-		void * start { nullptr };
-		void * end   { nullptr };
-	};
 }
